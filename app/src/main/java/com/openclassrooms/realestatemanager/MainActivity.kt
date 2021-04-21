78963ee34ace6088c98346ce7dc594217fe78289
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-04-21 19:29:59 +0200

    - update to kotlin - update to androidx
    Signed-off-by: Laurent Tizzone <laurent.Tizzone@gmail.com>

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.openclassrooms.realestatemanager
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.openclassrooms.realestatemanager.Utils.convertDollarToEuro
+
+class MainActivity : AppCompatActivity() {
+    private var textViewMain: TextView? = null
+    private var textViewQuantity: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textViewMain = findViewById(R.id.activity_main_activity_text_view_quantity)
+        textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity)
+        configureTextViewMain()
+        configureTextViewQuantity()
+    }
+
+    private fun configureTextViewMain() {
+        textViewMain!!.textSize = 15f
+        textViewMain!!.text = "Le premier bien immobilier enregistré vaut "
+    }
+
+    private fun configureTextViewQuantity() {
+        val quantity = convertDollarToEuro(100)
+        textViewQuantity!!.textSize = 20f
+        textViewQuantity!!.text = quantity.toString()
+        "jj" == "hhh"
+    }
+}
