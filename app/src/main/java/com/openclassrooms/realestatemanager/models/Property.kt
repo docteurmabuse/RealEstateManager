7684e24b78fdc65b0c0fee2ad4f92cf6f241efbf
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-04-24 18:45:35 +0200

    - Update Property.kt with entity
    Signed-off-by: Laurent Tizzone <laurent.Tizzone@gmail.com>

--- app/src/main/java/com/openclassrooms/realestatemanager/models/Property.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/models/Property.kt
@@ -1,25 +1,36 @@
 package com.openclassrooms.realestatemanager.models
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.util.*
 
 @Parcelize
+@Entity(tableName = "properties")
 data class Property(
+        @PrimaryKey(autoGenerate = true)
         val id: Int,
-        val type: String,
+        val type: String = "House",
         val price: Int,
         val surface: Int,
+        @ColumnInfo(name = "room_number")
         val roomNumber: Int,
-        val description: String,
+        val description: String?,
+        @ColumnInfo(name = "address_id")
         val addressId: Int,
-        val schools: Boolean,
-        val shops: Boolean,
-        val parcs: Boolean,
-        val stations: Boolean,
-        val hospital: Boolean,
+        val schools: Boolean?,
+        val shops: Boolean?,
+        val parcs: Boolean?,
+        val stations: Boolean?,
+        val hospital: Boolean?,
+        val museum: Boolean?,
         val sold: Boolean,
-        val entryDate: Date,
-        val saleDate: Date,
+        @ColumnInfo(name = "sell_date")
+        val sellDate: Date,
+        @ColumnInfo(name = "sold_date")
+        val soldDate: Date,
+        @ColumnInfo(name = "agent_id")
         val agentId: Int
 ) : Parcelable
