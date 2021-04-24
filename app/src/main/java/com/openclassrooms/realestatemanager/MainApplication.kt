d3368285b23eedea86e360c2a9f07103bbb66062
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-04-24 19:46:44 +0200

    - add onCreateMethod in MainApplication.kt
    Signed-off-by: Laurent Tizzone <laurent.Tizzone@gmail.com>

--- app/src/main/java/com/openclassrooms/realestatemanager/MainApplication.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainApplication.kt
@@ -2,6 +2,12 @@ package com.openclassrooms.realestatemanager
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import leakcanary.AppWatcher
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppWatcher.config = AppWatcher.config.copy(watchFragmentViews = false)
+    }
+}
