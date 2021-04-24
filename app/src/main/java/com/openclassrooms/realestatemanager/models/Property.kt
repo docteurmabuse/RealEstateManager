a63c14ebc885f25ee0b5063448beff44acde3a1c
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-04-24 16:23:36 +0200

    - Add builSrc & dependencies
    Signed-off-by: Laurent Tizzone <laurent.Tizzone@gmail.com>

--- app/src/main/java/com/openclassrooms/realestatemanager/models/Property.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/models/Property.kt
@@ -1,9 +1,5 @@
 package com.openclassrooms.realestatemanager.models
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class Property(
         val id: Int
-) : Parcelable
+)
