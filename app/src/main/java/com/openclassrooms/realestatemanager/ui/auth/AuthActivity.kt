0d1c974fb3d6c456db55daaba371e1789a26b6cd
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-04-26 20:50:22 +0200

    - update AuthActivity.kt -> update firebase code in order to not use deprecated method onActivityResult
    Signed-off-by: Laurent Tizzone <laurent.Tizzone@gmail.com>

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/auth/AuthActivity.kt
@@ -3,29 +3,44 @@ package com.openclassrooms.realestatemanager.ui.auth
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.AuthUI.IdpConfig
-import com.firebase.ui.auth.AuthUI.IdpConfig.*
+import com.firebase.ui.auth.AuthUI.IdpConfig.EmailBuilder
+import com.firebase.ui.auth.AuthUI.IdpConfig.GoogleBuilder
+import com.google.android.material.snackbar.Snackbar
+import com.openclassrooms.realestatemanager.MainActivity
 import com.openclassrooms.realestatemanager.R
-import com.openclassrooms.realestatemanager.utils.Constant.RC_SIGN_IN
-import java.util.*
+import com.openclassrooms.realestatemanager.databinding.ActivityAuthBinding
 
 
 class AuthActivity : AppCompatActivity() {
-    var resultLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val data: Intent? = result.data
-            }
-        }
+    private lateinit var binding: ActivityAuthBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        startSignInActivity()
+        setContentView(binding.root)
     }
+
+    var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                //  this.createUserInFirestore();
+                this.startMainActivity()
+                showSnackBar(binding.root, getString(R.string.connection_succeed))
+            }
+        }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
 
     private fun startSignInActivity() {
         //Login
@@ -35,20 +50,25 @@ class AuthActivity : AppCompatActivity() {
             .build()
 
         //  new AuthUI.IdpConfig.PhoneBuilder().build(),
-        val providers: List<IdpConfig> = Arrays.asList(
+        val providers: List<IdpConfig> = listOf(
             EmailBuilder().build(),
             GoogleBuilder().build()
         )
-        startActivityForResult(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAuthMethodPickerLayout(customLayout)
-                .setLogo(R.drawable.ic_logogo4lunch)
-                .setTheme(R.style.LoginTheme)
-                .setIsSmartLockEnabled(false, true)
-                .setAvailableProviders(providers) //EMAIL
-                .build(), RC_SIGN_IN
-        )
+
+        AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAuthMethodPickerLayout(customLayout)
+            //  .setTheme(R.style.LoginTheme)
+            .setIsSmartLockEnabled(false, true)
+            .setAvailableProviders(providers) //EMAIL
+            .build().apply {
+                resultLauncher.launch(this)
+            }
     }
 
+    private fun showSnackBar(view: View?, message: String?) {
+        if (view != null) {
+            Snackbar.make(view, message!!, Snackbar.LENGTH_SHORT).show()
+        }
+    }
 }
