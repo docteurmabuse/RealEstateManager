b11bb45b23878b8b061b395784b949b41e6e1649
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-04-29 19:28:54 +0200

    - create AgentEntityMapper
    Signed-off-by: Laurent Tizzone <laurent.Tizzone@gmail.com>

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/cache/model/AgentEntityMapper.kt
@@ -0,0 +1,23 @@
+package com.openclassrooms.realestatemanager.cache.model
+
+import com.openclassrooms.realestatemanager.domain.model.Agent
+import com.openclassrooms.realestatemanager.domain.model.util.DomainMapper
+
+class AgentEntityMapper : DomainMapper<AgentEntity, Agent> {
+    override fun mapToDomainModel(model: AgentEntity): Agent {
+        return Agent(
+            id = model.id,
+            name = model.name,
+            email = model.email
+        )
+    }
+
+    override fun mapFromDomainModel(domainModel: Agent): AgentEntity {
+        return AgentEntity(
+            id = domainModel.id,
+            name = domainModel.name,
+            email = domainModel.email
+        )
+    }
+
+}
