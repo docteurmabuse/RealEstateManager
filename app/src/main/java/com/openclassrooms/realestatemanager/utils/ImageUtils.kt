08d122b2fc54ca62d39b19074665c35cbf25b655
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-05-14 19:10:19 +0200

    - add several methods to ImageUtils.kt
    Signed-off-by: Laurent Tizzone <laurent.Tizzone@gmail.com>

--- app/src/main/java/com/openclassrooms/realestatemanager/utils/ImageUtils.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/utils/ImageUtils.kt
@@ -1,14 +1,22 @@
 package com.openclassrooms.realestatemanager.utils
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Build
 import android.os.Environment
 import java.io.File
 import java.io.IOException
+import java.io.InputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
 object ImageUtils {
 
+    //Returns an empty File in the app’s private pictures folder using a unique filename
     @Throws(IOException::class)
     fun createUniqueImageFile(context: Context): File {
         val timestamp = SimpleDateFormat("yyyyMMddHHmmss", Locale.US).format(Date())
@@ -17,7 +25,8 @@ object ImageUtils {
         return File.createTempFile(fileName, ".jpg", fileDir)
     }
 
-    private fun calculateInSampleSize(
+    //Calculate the optimum inSampleSize that can be used to resize an image to a specified width and height
+    fun calculateInSampleSize(
         width: Int,
         height: Int,
         reqWidth: Int,
@@ -32,5 +41,51 @@ object ImageUtils {
             }
         }
         return inSampleSize
+    }
+
+    //Downsampled image
+    fun decodeFileToSize(
+        filePath: String,
+        width: Int,
+        height: Int
+    ): Bitmap {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(filePath, options)
+        options.inSampleSize = calculateInSampleSize(
+            options.outWidth, options.outHeight, width, height
+        )
+        options.inJustDecodeBounds = false
+        return BitmapFactory.decodeFile(filePath, options)
+    }
+
+    // Rotate image
+    private fun rotateImage(img: Bitmap, degree: Float): Bitmap? {
+        val matrix = Matrix()
+        matrix.postRotate(degree)
+        val rotatedImg = Bitmap.createBitmap(img, 0, 0, img.width, img.height, matrix, true)
+        img.recycle()
+        return rotatedImg
+    }
+
+    //Check orientation in File meta data
+    @Throws(IOException::class)
+    fun rotateImgIfRequired(context: Context, img: Bitmap, selectedImage: Uri): Bitmap {
+        val input: InputStream? = context.contentResolver.openInputStream(selectedImage)
+        val path = selectedImage.path
+        val ei: ExifInterface = when {
+            Build.VERSION.SDK_INT > 23 && input != null -> ExifInterface(input)
+            path != null -> ExifInterface(path)
+            else -> null
+        } ?: return img
+        return when (ei.getAttributeInt(
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.ORIENTATION_NORMAL
+        )) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(img, 90.0f) ?: img
+            ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(img, 180.0f) ?: img
+            ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(img, 270.0f) ?: img
+            else -> img
+        }
     }
 }
