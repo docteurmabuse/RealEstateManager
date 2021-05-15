65a293de11e6ac4ec0b80171bb6abf09014e35cd
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-05-15 22:14:55 +0200

    - update CameraContract.kt
    Signed-off-by: Laurent Tizzone <laurent.Tizzone@gmail.com>

--- app/src/main/java/com/openclassrooms/realestatemanager/utils/CameraContract.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/utils/CameraContract.kt
@@ -1,51 +1,24 @@
 package com.openclassrooms.realestatemanager.utils
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.activity.result.contract.ActivityResultContract
-import androidx.core.content.FileProvider
 import java.io.File
 
 class CameraContract : ActivityResultContract<Int, File?>() {
     private var photoFile: File? = null
     val captureIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
-
     override fun createIntent(context: Context, input: Int?): Intent {
-        photoFile = null
-        try {
-            photoFile = ImageUtils.createUniqueImageFile(context)
-        } catch (ex: java.io.IOException) {
-
-        }
-        photoFile?.let { photoFile ->
-            val photoUri = FileProvider.getUriForFile(
-                context, "com.openclassrooms.realestatemanager.fileprovider",
-                photoFile
-            )
-            captureIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, photoUri)
-            val intentActivities = context.packageManager.queryIntentActivities(
-                captureIntent, PackageManager.MATCH_DEFAULT_ONLY
-            )
-            intentActivities.map {
-                it.activityInfo.packageName
-            }
-                .forEach {
-                    context.grantUriPermission(
-                        it,
-                        photoUri,
-                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION
-                    )
-                }
-        }
-        return captureIntent
+        TODO("Not yet implemented")
     }
 
     override fun parseResult(resultCode: Int, intent: Intent?): File? {
-        if (resultCode != Activity.RESULT_OK) {
-            return null
-        }
-        return photoFile
+        TODO("Not yet implemented")
     }
+
+    override fun getSynchronousResult(context: Context, input: Int?): SynchronousResult<File?>? {
+        return super.getSynchronousResult(context, input)
+    }
+
+
 }
