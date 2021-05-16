a60cf4c6155ac7e059cb3db294243e66e7cfbd14
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-05-16 10:56:45 +0200

    - create PhotosAdapter.kt
    Signed-off-by: Laurent Tizzone <laurent.Tizzone@gmail.com>

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/presentation/ui/adapters/PhotosAdapter.kt
@@ -0,0 +1,30 @@
+package com.openclassrooms.realestatemanager.presentation.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.realestatemanager.databinding.MediaItemContentBinding
+import com.openclassrooms.realestatemanager.domain.model.property.Media.Photo
+
+class PhotosAdapter(private val photos: ArrayList<Photo>) :
+    RecyclerView.Adapter<PhotosAdapter.PhotosViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): PhotosAdapter.PhotosViewHolder {
+        val binding =
+            MediaItemContentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PhotosViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PhotosAdapter.PhotosViewHolder, position: Int) {
+        val items = photos[position]
+        holder.binding.photo
+    }
+
+    override fun getItemCount(): Int = photos.size
+
+    inner class PhotosViewHolder(
+        val binding: MediaItemContentBinding
+    ) : RecyclerView.ViewHolder(binding.root)
+}
