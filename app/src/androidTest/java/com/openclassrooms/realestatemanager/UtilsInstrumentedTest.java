805509cfa94ec10b3942a6707bd6905560aa2041
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-05-21 15:47:06 +0200

    - add Response.kt pojo from mapbox geolocation to location model
    Signed-off-by: Laurent Tizzone <laurent.Tizzone@gmail.com>

---
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/UtilsInstrumentedTest.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.realestatemanager;
+
+public class UtilsInstrumentedTest {
+
+    //@Test
+    // @Throws(Exception::class)
+//    fun testIsInternetAvailable() {
+//        val context = ApplicationProvider.getApplicationContext<Context>()
+//        assertFalse(isInternetAvailable(context))
+//    }
+}
