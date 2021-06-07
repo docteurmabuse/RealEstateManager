222c3758dae5509bdd1278cdc0431bb4f85afa3b
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-06-07 14:38:26 +0200

    change way to deal with appbar menu and menu item click listener
    Signed-off-by: Laurent Tizzone <laurent.Tizzone@gmail.com>

--- app/src/main/java/com/openclassrooms/realestatemanager/presentation/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/presentation/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.openclassrooms.realestatemanager.presentation
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.activity.viewModels
+import androidx.annotation.MenuRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -19,7 +27,6 @@ import com.openclassrooms.realestatemanager.databinding.ActivityMainBinding
 import com.openclassrooms.realestatemanager.domain.model.agent.Agent
 import com.openclassrooms.realestatemanager.domain.model.data.DataState
 import com.openclassrooms.realestatemanager.domain.model.property.Property
-import com.openclassrooms.realestatemanager.presentation.ui.BaseActivity
 import com.openclassrooms.realestatemanager.presentation.ui.ItemTabsFragmentDirections
 import com.openclassrooms.realestatemanager.presentation.ui.MainViewModel
 import com.openclassrooms.realestatemanager.presentation.ui.agents.AgentsViewModel
@@ -35,7 +42,8 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity constructor(
     private var properties: List<Property>? = null
-) : BaseActivity() {
+) : AppCompatActivity(), Toolbar.OnMenuItemClickListener,
+    NavController.OnDestinationChangedListener {
     @Inject
     lateinit var fragmentFactory: MainFragmentFactory
     private lateinit var binding: ActivityMainBinding
@@ -49,7 +57,13 @@ class MainActivity constructor(
     private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as MainNavHostFragment }
     private val navController by lazy { navHostFragment.navController }
     private val appBarConfiguration by lazy { AppBarConfiguration(navController.graph) }
+    private var isFabOpen = false
 
+    val currentNavigationFragment: Fragment?
+        get() = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main)
+            ?.childFragmentManager
+            ?.fragments
+            ?.first()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,10 +72,48 @@ class MainActivity constructor(
         setupActionBarWithNavController(navController, appBarConfiguration)
         viewModel.fetchProperties()
         setObserver()
+        setupBottomNavigationAndFab()
         setAddPropertyFabListener()
         setAddAgentFabListener()
     }
 
+    private fun setupBottomNavigationAndFab() {
+        // Wrap binding.run to ensure ContentViewBindingDelegate is calling this Activity's
+        // setContentView before accessing views
+        binding.run {
+            navController.addOnDestinationChangedListener(
+                this@MainActivity
+            )
+        }
+
+        // Set up the BottomAppBar menu
+        binding.bottomAppBar.apply {
+            setNavigationOnClickListener {
+            }
+            setOnMenuItemClickListener(this@MainActivity)
+        }
+
+        // Set a custom animation for showing and hiding the FAB
+        /*binding..apply {
+            setShowMotionSpecResource(R.animator.fab_show)
+            setHideMotionSpecResource(R.animator.fab_hide)
+            setOnClickListener {
+                navigateToCompose()
+            }
+        }*/
+    }
+
+    private fun setBottomAppBarForHome(@MenuRes menuRes: Int) {
+        binding.run {
+            // expandableFabLayout.setImageState(intArrayOf(-android.R.attr.state_activated), true)
+            bottomAppBar.visibility = View.VISIBLE
+            bottomAppBar.replaceMenu(menuRes)
+            expandableFabLayout.contentDescription = getString(R.string.hello_blank_fragment)
+            expandableFabLayout.visibility = View.VISIBLE
+            bottomAppBar.performShow()
+            expandableFabLayout.isShown
+        }
+    }
 
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
         super.onSaveInstanceState(savedInstanceState)
@@ -79,8 +131,44 @@ class MainActivity constructor(
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_filter_properties -> {
+                navController.navigate(R.id.action_itemTabsFragment2_to_propertySearchDialogFragment)
+                Timber.d("FILTER: filter ok")
+            }
+        }
+        return true
+    }
 
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        when (destination.id) {
+            R.id.itemTabsFragment2 -> {
+                setBottomAppBarForHome(getBottomAppBarMenuDestination(destination))
+            }
+            R.id.propertySearchDialogFragment -> {
+                setBottomAppBarForHome(getBottomAppBarMenuDestination(destination))
+                hideBottomAppBar()
+            }
+        }
+    }
+
+    private fun getBottomAppBarMenuDestination(destination: NavDestination? = null): Int {
+        val dest = destination
+            ?: findNavController(R.id.nav_host_fragment_activity_main).currentDestination
+        return when (dest?.id) {
+            R.id.itemTabsFragment2 -> R.menu.menu_fragment_properties
+            R.id.propertySearchDialogFragment -> R.menu.edit_menu
+
+            else -> R.menu.bottom_nav_menu
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val searchItem = binding.bottomAppBar.menu?.findItem(R.id.action_search)
         val searchView = searchItem?.actionView as SearchView
         searchView.onQueryTextChanged {
@@ -90,39 +178,63 @@ class MainActivity constructor(
         return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_sort_by_name -> {
-                true
-            }
-            R.id.action_sort_by_date_on_market -> {
-                true
-            }
+    private fun hideBottomAppBar() {
+        binding.run {
+            bottomAppBar.performHide()
+            // Get a handle on the animator that hides the bottom app bar so we can wait to hide
+            // the fab and bottom app bar until after it's exit animation finishes.
+            bottomAppBar.animate().setListener(object : AnimatorListenerAdapter() {
+                var isCanceled = false
+                override fun onAnimationEnd(animation: Animator?) {
+                    if (isCanceled) return
 
-            R.id.action_search -> {
-                true
-            }
-            android.R.id.home -> {
-                onBackPressed()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
+                    // Hide the BottomAppBar to avoid it showing above the keyboard
+                    // when composing a new email.
+                    bottomAppBar.visibility = View.GONE
+                    expandableFabLayout.visibility = View.INVISIBLE
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {
+                    isCanceled = true
+                }
+            })
         }
     }
 
-    override fun onBackPressed() {
-        //Execute your code here
-        binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_sort_by_name ->
+                Timber.d("SEARCH:CLICKED")
 
-        val navHostFragment = findNavController(R.id.nav_host_fragment_activity_main)
-        navHostFragment.navigate(R.id.mainActivity)
+
+            R.id.action_sort_by_date_on_market -> {
+
+            }
+
+            R.id.action_search ->
+                Timber.d("SEARCH:CLICKED")
+
+
+            R.id.action_filter_properties ->
+                Timber.d("FILTER:CLICKED")
+
+            android.R.id.home -> {
+                onBackPressed()
+            }
+
+        }
+        return true
+    }
+
+    override fun onBackPressed() {
+        binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+        navController.navigate(R.id.mainActivity)
     }
 
 
     private fun setAddAgentFabListener() {
 
         binding.fabAddAgent.setOnClickListener {
-            val navHostFragment = findNavController(R.id.nav_host_fragment_activity_main)
             isAddAgentView = true
             isAddPropertyView = false
 
@@ -135,15 +247,13 @@ class MainActivity constructor(
                 binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
                 binding.expandableFabLayout.visibility = View.GONE
             }
-
-            navHostFragment.navigate(action)
+            navController.navigate(action)
         }
     }
 
 
     private fun setAddPropertyFabListener() {
         binding.fabAddProperty.setOnClickListener {
-            val navHostFragment = findNavController(R.id.nav_host_fragment_activity_main)
             isAddAgentView = false
             isAddPropertyView = true
 
@@ -158,8 +268,7 @@ class MainActivity constructor(
                 binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
                 binding.expandableFabLayout.visibility = View.GONE
             }
-
-            navHostFragment.navigate(action)
+            navController.navigate(action)
         }
     }
 
@@ -212,5 +321,6 @@ class MainActivity constructor(
         properties = list
         Timber.tag("MAP").d("MAP_PROPERTIES: ${properties!!.size}")
     }
+
 
 }
