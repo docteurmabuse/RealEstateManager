1d133b5c27f1e7fd608afeeb51bf6b1d87ec96ab
Laurent Tizzone <laurent.Tizzone@gmail.com>
2021-06-25 14:51:56 +0200

    update MainViewModel.kt maxprice
    Signed-off-by: Laurent Tizzone <laurent.Tizzone@gmail.com>

--- app/src/main/java/com/openclassrooms/realestatemanager/presentation/ui/MainViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/presentation/ui/MainViewModel.kt
@@ -51,7 +51,7 @@ constructor(
     var loft = MutableLiveData<Boolean>(true)
     var manor = MutableLiveData<Boolean>(true)
 
-    var maxPrice = MutableLiveData<Float>(1000F)
+    var maxPrice = MutableLiveData<Float>(100000000F)
     var minPrice = MutableLiveData<Float>(0F)
     var stepSize = MutableLiveData<Float>(1F)
 
@@ -287,30 +287,13 @@ constructor(
 
     @ExperimentalCoroutinesApi
     fun resetFilters() {
-        val minSurface: Float? =
-            if (surfaceArray.value.isNullOrEmpty())
-                0F else surfaceArray.value?.get(0)
-
-        val maxSurface: Float? =
-            if (surfaceArray.value.isNullOrEmpty())
-                1000F
-            else surfaceArray.value?.get(1)
-
-        val minPrice: Float? =
-            if (priceArray.value.isNullOrEmpty())
-                0F else priceArray.value?.get(0)
-
-        val maxPrice: Float? =
-            if (priceArray.value.isNullOrEmpty())
-                1000F
-            else priceArray.value?.get(1)
         house.value = true
         flat.value = true
         duplex.value = true
         penthouse.value = true
         manor.value = true
-        priceArray.value = arrayOf(minPrice!!, maxPrice!!)
-        surfaceArray.value = arrayOf(minSurface!!, maxSurface!!)
+        priceArray.value = arrayOf(minPrice.value!!, maxPrice.value!!)
+        surfaceArray.value = arrayOf(minSurface.value!!, maxSurface.value!!)
         typeList.value = arrayListOf("House", "Flat", "Duplex", "Penthouse", "Manor", "Loft")
         roomNumber.value = 1F
         bathroomNumber.value = 0F
